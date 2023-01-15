a0a11b6b04c7e777110e902131882acf58236d8c
mmaad18 <61145345+mmaad18@users.noreply.github.com>
2023-01-15 19:56:34 +0100

    Initial commit :D

---
+++ src/initialSetup.sh
@@ -0,0 +1,7 @@
+
+sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+sudo apt install git -y
+sudo apt install curl -y
+sudo apt install ansible -y
+
